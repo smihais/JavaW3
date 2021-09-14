3898f39d79a9e9524cdc5b832586a5d160e7b920
smihais <samoilams@gmail.com>
2021-09-14 21:47:30 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-	String name = "Mihai";
-	System.out.println("Your name is: "+name);
-	int age = 44;
-	System.out.println("Your age is: "+age);
-  }
-}
